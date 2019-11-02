aa1a02bc024bb20c96f3afb0499770f32ed59a26
Michael LaRoy <mike.laroy@gmail.com>
2019-11-02 12:36:27 -0700

    Create index.js
    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const observeEntry = config => {
+
+    const defaults = {
+        intersectingClass: 'is-intersected',
+        root: null,
+        rootMargin: '0px',
+        threshold: 1.0
+    }
+
+    let options;
+
+    if ( typeof config == 'string') {
+        defaults.selector = config;
+        options = { ...defaults };
+    } else {
+        options = { ...defaults, ...config };
+    }
+
+    const els = document.querySelectorAll( options.selector );
+
+    const callback = entries => {
+        entries.forEach( entry => {
+            if ( entry.isIntersecting ) {
+                entry.target.classList.add( options.intersectingClass );
+            }
+        });
+    }
+
+    const observer = new IntersectionObserver( callback, options );
+
+    [].forEach.call(els, el => observer.observe( el ));
+
+    return observer;
+}
+
+export default observeEntry;
